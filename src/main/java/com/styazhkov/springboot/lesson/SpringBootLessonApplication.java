a842bb54c7acd0b5d17f2f8490705f72b29b7c61
Alex Snow <32740603+styazhkoV@users.noreply.github.com>
2020-12-14 00:38:10 +0600

    Сконфигурирован spring https://www.youtube.com/watch?v=8Z5bA4x5dPE&list=PLnh8EajVFTl73sJch9bJtPLnsNkTrFPbO&index=2

---
+++ src/main/java/com/styazhkov/springboot/lesson/SpringBootLessonApplication.java
@@ -0,0 +1,12 @@
+package com.styazhkov.springboot.lesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLessonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLessonApplication.class,args);
+
+    }
+}
